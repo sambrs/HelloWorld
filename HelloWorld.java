22d4268582fe9cc2a2d66a810bb1407baefb0adf
sambrs <samadhanborse9669@gmail.com>
2021-06-11 15:44:49 +0530

    updated print
    updated print

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
-		System.out.println("My First App12");
+		System.out.println("Successfully containerize the and image is created");
 	}
 }
